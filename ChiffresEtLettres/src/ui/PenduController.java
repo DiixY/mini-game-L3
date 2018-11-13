dbde263934f0073573ceaf6fb3fd05e6cacf58ee
Younès <younes59220@hotmail.fr>
2018-11-13 16:57:49 +0100

    Ajout bouton retour menu

--- ChiffresEtLettres/src/ui/PenduController.java
+++ ChiffresEtLettres/src/ui/PenduController.java
@@ -1,16 +1,22 @@
 package ui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import games.Pendu;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
 public class PenduController implements Initializable {
 	
@@ -180,6 +186,15 @@ public class PenduController implements Initializable {
 			chances.setText("Chance(s) restante(s) : "+ p.getCuTry());
 	}
 
+	@FXML
+	public void goGames(ActionEvent event) throws IOException
+	{	
+			Parent playParent = FXMLLoader.load(getClass().getResource("PlayView.fxml"));
+			Scene scene = new Scene(playParent);
+			Stage playwindow = (Stage) (((Node) event.getSource()).getScene().getWindow());
+			
+			playwindow.setScene(scene);	
+	}
 	
 	public void errAlert(char c)
 	{
