24cff2e55106605cb2fd03460f55668d388608a8
Younès <Youns@Younes-Bureau>
2018-12-08 19:18:55 +0100

    Update Interface - 7

--- ChiffresEtLettres/src/ui/Main.java
+++ ChiffresEtLettres/src/ui/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws IOException {
 		
 		this.primaryStage = primaryStage;
-		Font.loadFont("/Res/Arvin.ttf", 12);
+		Font.loadFont("Res/Arvin.ttf", 12);
 		Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
 		this.primaryStage.setTitle("miniGames");
 		
